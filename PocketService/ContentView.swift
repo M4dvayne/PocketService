810bab941eaf90b032779a3b5db5119907a2883f
KremnevViacheslav <rusted2192@gmail.com>
2022-05-16 02:54:46 +0300

    Initial Commit

---
+++ PocketService/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PocketService
+//
+//  Created by Вячеслав Кремнев on 16.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PocketService/PocketServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketServiceApp.swift
+//  PocketService
+//
+//  Created by Вячеслав Кремнев on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct PocketServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
